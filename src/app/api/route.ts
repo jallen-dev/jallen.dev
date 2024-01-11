7de3936de8fd10b7aa4b0d3271ebdc10ac4e2649
Justin Allen <jallen@jallen.dev>
2024-01-11 02:21:02 -0500

    Add test api

---
+++ src/app/api/route.ts
@@ -0,0 +1,8 @@
+export const runtime = "edge";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const name = searchParams.get("name") || "World";
+
+  return Response.json({ message: `Hello ${name}` });
+}
