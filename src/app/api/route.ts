11ca801581eca37b376c480173cc4e38358e3990
Justin Allen <jallen@jallen.dev>
2024-01-12 15:03:57 -0500

    Cleanup

--- src/app/api/route.ts
+++
@@ -1,8 +0,0 @@
-export const runtime = "edge"
-
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url)
-  const name = searchParams.get("name") || "World"
-
-  return Response.json({ message: `Hello ${name}` })
-}
